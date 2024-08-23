58c0344e2ffd444f6be761f596db091777b57e1c
lzx8035 <lzx8035@gmail.com>
2024-08-22 21:30:23 -0700

    A New Way Of Implementing Routes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+# This Project is for learning React Router with data loading
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    // loader: Loader,
+    // children: [
+    //   {
+    //     path: "team",
+    //   },
+    // ],
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderID",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  const lea = "";
-  return <div>Hello vite</div>;
+  return <RouterProvider router={router}>Hello vite</RouterProvider>;
 }
 
 export default App;
