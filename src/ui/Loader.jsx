4cde7344f6bf7ec6e4b25da249517e8eac6d3bd9
lzx8035 <lzx8035@gmail.com>
2024-08-29 21:22:49 -0700

    Style Loader and Configure and Customize Tailwind

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,17 @@
 function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loader;
+
+// inset-0:
+// {
+//   top: 0;
+//   right: 0;
+//   bottom: 0;
+//   left: 0;
+// }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      pizza: "Roboto Mono, monospace",
+      // pizza: "Handjet",
+    },
+    extend: {
+      colors: {
+        pizza: "#F6E6BC",
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
